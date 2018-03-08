f7755ead538717c1cdd5ca16a2e307a0cad7fe4a
Vincibean <andrebessi00@gmail.com>
2018-03-09 00:38:56 +0100

    Turned CardSet into Hand

--- src/main/scala/org/vincibean/game/set/lexical/Hand.scala
+++ src/main/scala/org/vincibean/game/set/lexical/Hand.scala
@@ -1,4 +1,4 @@
 package org.vincibean.game.set.lexical
 
 // Enforce all sets to consist of only three cards
-case class CardSet(c1: Card, c2: Card, c3: Card)
+case class Hand(c1: Card, c2: Card, c3: Card)
