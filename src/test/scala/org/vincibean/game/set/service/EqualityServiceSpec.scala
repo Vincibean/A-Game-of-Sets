3e1e6827ed4fc2d48f471404b908bc1f18822349
Vincibean <andrebessi00@gmail.com>
2018-03-09 21:14:45 +0100

    Turned EqualityServiceSpec's tests into specs

--- src/test/scala/org/vincibean/game/set/service/EqualityServiceSpec.scala
+++ src/test/scala/org/vincibean/game/set/service/EqualityServiceSpec.scala
@@ -1,47 +1,81 @@
 package org.vincibean.game.set.service
 
-import org.specs2.Specification
+import org.scalacheck.{Arbitrary, Gen, Prop}
 import org.specs2.matcher.MatchResult
+import org.specs2.scalacheck.{
+  ScalaCheckFunction1,
+  ScalaCheckFunction2,
+  ScalaCheckFunction3
+}
 import org.specs2.specification.core.SpecStructure
+import org.specs2.{ScalaCheck, Specification}
 import org.vincibean.game.set.lexical._
 
-class EqualityServiceSpec extends Specification with EqualityService {
+class EqualityServiceSpec
+    extends Specification
+    with ScalaCheck
+    with EqualityService {
   override def is: SpecStructure =
     s2"""
         equal3 can
-         test the equality of primitives (Ints) ${equal3(1, 1, 1) must beTrue}
-         fail with different primitives (Ints) ${equal3(1, 2, 3) must beFalse}
-         fail with 'almost' equal primitives (Ints) ${equal3(1, 1, 3) must beFalse}
-         test the equality of instances (Strings) ${equal3("", "", "") must beTrue}
-         fail with different instances (Strings) ${equal3("1", "2", "3") must beFalse}
-         fail with 'almost' equal instances (Strings) ${equal3("", "", "1") must beFalse}
-         test the equality of objects (Colors) ${equal3(Red, Red, Red) must beTrue}
-         fail with different objects (Colors) ${equal3(Red, Green, Purple) must beFalse}
-         fail with 'almost' equal objects (Colors) ${equal3(Red, Purple, Red) must beFalse}
-         test the equality of instances (Cards) $e1
-         fail with different instances (Cards) $e2
-         fail with 'almost' equal instances (Cards) $e3
+         test the equality of primitives (Ints) ${equalityOf[Int]}
+         fail with 'almost' equal primitives (Ints) ${almostEqualityOf[Int]}
+         fail with different primitives (Ints) ${disequalityOf[Int]}
+         test the equality of instances (Strings) ${equalityOf[String]}
+         fail with 'almost' equal instances (Strings) ${almostEqualityOf[String]}
+         fail with different instances (Strings) ${disequalityOf[String]}
+         test the equality of objects (Colors) ${equalityOf[Color]}
+         fail with 'almost' equal objects (Colors) ${almostEqualityOf[Color]}
+         fail with different objects (Colors) ${disequalityOf[Color]}
+         test the equality of instances (Cards) ${equalityOf[Card]}
+         fail with 'almost' equal instances (Cards) ${almostEqualityOf[Card]}
+         fail with different instances (Cards) ${disequalityOf[Card]}
       """
 
-  def e1: MatchResult[Boolean] = {
-    val c1 = Card(Ovals, Red, One, Solid)
-    val c2 = Card(Ovals, Red, One, Solid)
-    val c3 = Card(Ovals, Red, One, Solid)
-    equal3(c1, c2, c3) must beTrue
+  def equalityOf[T](implicit ev: Arbitrary[T])
+    : ScalaCheckFunction1[T, MatchResult[Boolean]] = {
+    prop { (i: T) =>
+      equal3(i, i, i) must beTrue
+    }
   }
 
-  def e2: MatchResult[Boolean] = {
-    val c1 = Card(Ovals, Red, One, Solid)
-    val c2 = Card(Squiggles, Purple, Two, Outline)
-    val c3 = Card(Diamonds, Green, Three, Striped)
-    equal3(c1, c2, c3) must beFalse
+  def almostEqualityOf[T](
+      implicit ev: Arbitrary[T]): ScalaCheckFunction2[T, T, Prop] = {
+    prop { (x: T, y: T) =>
+      (x != y) ==> {
+        equal3(x, y, y) must beFalse
+      }
+    }
   }
 
-  def e3: MatchResult[Boolean] = {
-    val c1 = Card(Ovals, Red, One, Solid)
-    val c2 = Card(Squiggles, Purple, Two, Outline)
-    val c3 = Card(Ovals, Red, One, Solid)
-    equal3(c1, c2, c3) must beFalse
+  def disequalityOf[T](
+      implicit ev: Arbitrary[T]): ScalaCheckFunction3[T, T, T, Prop] = {
+    prop { (x: T, y: T, z: T) =>
+      ((x != y) && (y != z) && (x != z)) ==> {
+        equal3(x, y, z) must beFalse
+      }
+    }
+  }
+
+  implicit val shapes: Arbitrary[Shape] = Arbitrary(
+    Gen.oneOf(Ovals, Diamonds, Squiggles))
+
+  implicit val colors: Arbitrary[Color] = Arbitrary(
+    Gen.oneOf(Red, Purple, Green))
+
+  implicit val numbers: Arbitrary[Number] = Arbitrary(
+    Gen.oneOf(One, Two, Three))
+
+  implicit val shadings: Arbitrary[Shading] = Arbitrary(
+    Gen.oneOf(Solid, Striped, Outline))
+
+  implicit val cards: Arbitrary[Card] = Arbitrary {
+    for {
+      s <- Arbitrary.arbitrary[Shape]
+      c <- Arbitrary.arbitrary[Color]
+      n <- Arbitrary.arbitrary[Number]
+      sd <- Arbitrary.arbitrary[Shading]
+    } yield Card(s, c, n, sd)
   }
 
 }
