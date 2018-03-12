5de04eaf001e76d3c465de4821d35e52b971ae82
Vincibean <andrebessi00@gmail.com>
2018-03-12 01:40:35 +0100

    Improved DeckService and corresponding tests

--- src/main/scala/org/vincibean/game/set/service/DeckService.scala
+++ src/main/scala/org/vincibean/game/set/service/DeckService.scala
@@ -1,10 +1,12 @@
 package org.vincibean.game.set.service
 
+import eu.timepit.refined.auto._
 import org.vincibean.game.set.lexical._
+import org.vincibean.game.set.{Natural, PositiveNatural}
 
 import scala.util.Random
 
-trait DeckService {
+trait DeckService extends DifferenceService {
 
   val singleDeck: Seq[Card] = for {
     shape <- Shape.values
@@ -14,18 +16,24 @@ trait DeckService {
   } yield Card(shape, color, number, shading)
 
   // TODO shuffle
-  def deckFrom(decksNum: Int): Deck = {
+  def deckFrom(decksNum: Natural): Deck = {
     val decks =
       (0 until decksNum).map(_ => singleDeck).foldLeft(Seq.empty[Card])(_ ++ _)
     Deck(Random.shuffle(decks))
   }
 
-  def layDownCards(deck: Deck, num: Int): (Deck, Deck) = {
+  def layDownCards(deck: Deck, num: PositiveNatural): (Deck, Deck) = {
     val (laidDown, remaining) = deck.cards.splitAt(num)
     (Deck(laidDown), Deck(remaining))
   }
 
-  def pickCards(deck: Deck)(c1: Int, c2: Int, c3: Int): Hand =
-    Hand(deck.cards(c1), deck.cards(c2), deck.cards(c3))
+  def pickCards(
+      deck: Deck)(c1: Natural, c2: Natural, c3: Natural): Option[Hand] =
+    for {
+      card1 <- deck.cards.findAt(c1)
+      card2 <- deck.cards.findAt(c2)
+      card3 <- deck.cards.findAt(c3)
+      if totallyDifferent3(c1, c2, c3)
+    } yield Hand(card1, card2, card3)
 
 }
--- src/test/scala/org/vincibean/game/set/service/DeckServiceSpec.scala
+++ src/test/scala/org/vincibean/game/set/service/DeckServiceSpec.scala
@@ -1,76 +1,82 @@
 package org.vincibean.game.set.service
 
-import org.scalacheck.{Arbitrary, Prop}
+import eu.timepit.refined.auto._
+import eu.timepit.refined.numeric._
+import eu.timepit.refined.scalacheck.NumericInstances
+import org.scalacheck.Arbitrary
 import org.specs2.matcher.MatchResult
-import org.specs2.scalacheck.{
-  ScalaCheckFunction1,
-  ScalaCheckFunction2,
-  ScalaCheckFunction3
-}
+import org.specs2.scalacheck.ScalaCheckFunction3
 import org.specs2.specification.core.SpecStructure
 import org.specs2.{ScalaCheck, Specification}
-import org.vincibean.game.set.ArbitraryCards
 import org.vincibean.game.set.lexical._
+import org.vincibean.game.set.{ArbitraryCards, Natural}
 
 class DeckServiceSpec
     extends Specification
     with ScalaCheck
+    with NumericInstances
     with DeckService
     with ArbitraryCards {
   override def is: SpecStructure =
     s2"""
         deckFrom must
-          return an empty deck if a number less than or equal to 0 is given $p1
-          return a full (with the appropriate number of cards) deck if a number greaten than 0 is given $e2
+          return a full (with the appropriate number of cards) deck if a number greaten than 0 is given $e1
         layDownCards must
-          use the right cardinality for laid down cards as well as remaining cards in the Deck $e3
+          use the right cardinality for laid down cards as well as remaining cards in the Deck $e2
+          consume the whole Deck if a big number is provided $e3
         pickCards must
           return the expected cards $p4
+          return None if big numbers are provided $p5
+          return None if the same number is provided $p6
+          return None if two same numbers are provided $p7
       """
 
-  def p1: ScalaCheckFunction1[Int, Prop] = prop { (i: Int) =>
-    (i <= 0) ==> {
-      deckFrom(i).cards must beEmpty
-    }
-  }
+  def e1: MatchResult[Seq[Card]] = deckFrom(1).cards must haveSize(81)
 
-  // TODO
-  def p2: ScalaCheckFunction1[Int, Prop] = prop { (i: Int) =>
-    (i > 0) ==> {
-      deckFrom(i).cards must haveSize(i * 81)
-    }
-  }
+  implicit val arbitraryDeck: Arbitrary[Deck] = Arbitrary(
+    Arbitrary.arbitrary[Natural].map(deckFrom))
 
-  def e2: MatchResult[Seq[Card]] = deckFrom(1).cards must haveSize(81)
-
-  implicit val arbitraryDeck: Arbitrary[Deck] = Arbitrary {
-    for {
-      i <- Arbitrary.arbitrary[Int]
-      if i > 0
-    } yield deckFrom(i)
-  }
-
-  // TODO
-  def p3: ScalaCheckFunction2[Int, Deck, Prop] = prop { (i: Int, d: Deck) =>
-    ((i > 0) && (i <= d.cards.length)) ==> {
-      val (laidDown, remaining) = layDownCards(Deck(singleDeck), i)
-      (laidDown.cards must haveSize(i)) and (remaining.cards must haveSize(
-        singleDeck.length - i))
-    }
-  }
-
-  def e3: MatchResult[Seq[Card]] = {
+  def e2: MatchResult[Seq[Card]] = {
     val (laidDown, remaining) = layDownCards(Deck(singleDeck), 12)
     (laidDown.cards must haveSize(12)) and (remaining.cards must haveSize(
       singleDeck.length - 12))
   }
 
-  def p4: ScalaCheckFunction3[Card, Card, Card, MatchResult[Card]] = prop {
-    (c1: Card, c2: Card, c3: Card) =>
-      val d = Deck(Seq(c1, c2, c3))
-      val h = pickCards(d)(0, 1, 2)
-      (h.c1 must beEqualTo(c1)) and (h.c2 must beEqualTo(c2)) and (h.c3 must beEqualTo(
-        c3))
+  def e3: MatchResult[Seq[Card]] = {
+    val (laidDown, remaining) = layDownCards(Deck(singleDeck), 999)
+    (laidDown.cards must haveSize(singleDeck.length)) and (remaining.cards must beEmpty)
   }
+
+  def p4: ScalaCheckFunction3[Card, Card, Card, MatchResult[Option[Hand]]] =
+    prop { (c1: Card, c2: Card, c3: Card) =>
+      val d = Deck(Seq(c1, c2, c3))
+      val sh = pickCards(d)(0, 1, 2)
+      sh must beSome.like {
+        case (h: Hand) =>
+          (h.c1 must beEqualTo(c1)) and (h.c2 must beEqualTo(c2)) and (h.c3 must beEqualTo(
+            c3))
+      }
+    }
+
+  def p5: ScalaCheckFunction3[Card, Card, Card, MatchResult[Option[Hand]]] =
+    prop { (c1: Card, c2: Card, c3: Card) =>
+      val d = Deck(Seq(c1, c2, c3))
+      val sh = pickCards(d)(90, 91, 92)
+      sh must beNone
+    }
+
+  def p6: ScalaCheckFunction3[Card, Card, Card, MatchResult[Option[Hand]]] =
+    prop { (c1: Card, c2: Card, c3: Card) =>
+      val d = Deck(Seq(c1, c2, c3))
+      val sh = pickCards(d)(0, 0, 0)
+      sh must beNone
+    }
+
+  def p7: ScalaCheckFunction3[Card, Card, Card, MatchResult[Option[Hand]]] =
+    prop { (c1: Card, c2: Card, c3: Card) =>
+      val d = Deck(Seq(c1, c2, c3))
+      val sh = pickCards(d)(0, 0, 1)
+      sh must beNone
+    }
 
 }
